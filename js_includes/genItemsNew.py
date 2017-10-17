a5a5ad39a2821dd7d305276b3a3d01e825db4451
Zaroukian <1503928007@mil>
2017-10-17 16:58:30 -0400

    update

--- js_includes/genItemsNew.py
+++ js_includes/genItemsNew.py
@@ -2,149 +2,157 @@
 
 import csv, string
 
-ont0A = "http://i1341.photobucket.com/albums/o753/ezaroukian/0A_zpslpinf2fi.png"
-ont0Al = "http://i1341.photobucket.com/albums/o753/ezaroukian/0Al_zps1awoxvuz.png"
-ont0B = "http://i1341.photobucket.com/albums/o753/ezaroukian/0B_zpsk5dn4xfl.png"
-ont0Bl = "http://i1341.photobucket.com/albums/o753/ezaroukian/0Bl_zpsnhfil1sm.png"
-ont0C = "http://i1341.photobucket.com/albums/o753/ezaroukian/0C_zpsgdjlzzvp.png"
-ont0Cl = "http://i1341.photobucket.com/albums/o753/ezaroukian/0Cl_zpsclmfmgrs.png"
-ont2A = "http://i1341.photobucket.com/albums/o753/ezaroukian/2A_zpsupnzz2ms.png"
-ont2Al = "http://i1341.photobucket.com/albums/o753/ezaroukian/2Al_zpswfhoaulk.png"
-ont2B = "http://i1341.photobucket.com/albums/o753/ezaroukian/2B_zpsjejd6spx.png"
-ont2Bl = "http://i1341.photobucket.com/albums/o753/ezaroukian/2Bl_zpsk0rsjoio.png"
-ont2C = "http://i1341.photobucket.com/albums/o753/ezaroukian/2C_zps3jmrm5lm.png"
-ont2Cl = "http://i1341.photobucket.com/albums/o753/ezaroukian/2Cl_zpsj0pk83ox.png"
-ont3A = "http://i1341.photobucket.com/albums/o753/ezaroukian/3A_zpsem2bpmkm.png"
-ont3Al = "http://i1341.photobucket.com/albums/o753/ezaroukian/3Al_zpsbxkom9x9.png"
-ont3B = "http://i1341.photobucket.com/albums/o753/ezaroukian/3B_zpssdog5hbw.png"
-ont3Bl = "http://i1341.photobucket.com/albums/o753/ezaroukian/3Bl_zpsxsupqx7c.png"# "http://i1341.photobucket.com/albums/o753/ezaroukian/3Bl_zpsepjkv9je.png"
-ont3C = "http://i1341.photobucket.com/albums/o753/ezaroukian/3C_zpsa2jieegn.png"
-ont3Cl = "http://i1341.photobucket.com/albums/o753/ezaroukian/3Cl_zpsp2ljm9qv.png"
 
+#Build up regular items (I might need to put the scenario in the group name...)
+group1=group2=group3=group4=group5=group6=group7 = "noGroup"
 
+def writeItems():
+        items = [[group7+".TrainingB", "MessageForm", { "html": { "include": "TrainingB."+group7+".html" }, "answers": { "include": "TrainingB.ans.html"},}],#NEED ORIG VERSIONS!
+                  [group2+".Scenario1", "MessageForm", { "html": { "include": "Scenario1."+group2+".html" }, "answers": { "include": "Scenario1.ans.html"},}],
+                  [group3+".Scenario2", "MessageForm", { "html": { "include": "Scenario2."+group3+".html" }, "answers": { "include": "Scenario2.ans.html"},}],     
+                  [group4+".Scenario4", "MessageForm", { "html": { "include": "Scenario4."+group4+".html" }, "answers": { "include": "Scenario4.ans.html"},}],
+                  [group5+".Scenario5", "MessageForm", { "html": { "include": "Scenario5."+group5+".html" }, "answers": { "include": "Scenario5.ans.html"},}],
+                  [group6+".Scenario7", "MessageForm", { "html": { "include": "Scenario7."+group6+".html" }, "answers": { "include": "Scenario7.ans.html"},}],
+                  [group1+".Scenario8", "MessageForm", { "html": { "include": "Scenario8."+group1+".html" }, "answers": { "include": "Scenario8.ans.html"},}]]
+        return items
+               
+#in case I want to change the names for different versions                         
+pipeline = "ext"
+plain = "orig"
 
-with open('ontSent.csv', 'rb') as csvfile:
-        counter = 0
-        items = []
-        itemsNames0n = [] #practice
-        itemsNames0c = [] #practice
-        itemsNames1n = [] #block 1
-        itemsNames1c = []
-        itemsNames2n = [] #block 2
-        itemsNames2c = []
-        r = csv.reader(csvfile)
-        r.next()
-        for row in r:
-                counter += 1
-                ontograph = row[0]
-##                if row[0]=="0Al":
-##                        ontLink = ont0Al
-##                elif row[0]=="0Bl":
-##                        ontLink = ont0Bl
-##                elif row[0]=="0Cl":
-##                        ontLink = ont0Cl
-##                elif row[0]=="2Al":
-##                        ontLink = ont2Al
-##                elif row[0]=="2Bl":
-##                        ontLink = ont2Bl
-##                elif row[0]=="2Cl":
-##                        ontLink = ont2Cl
-##                elif row[0]=="3Al":
-##                        ontLink = ont3Al
-##                elif row[0]=="3Bl":
-##                        ontLink = ont3Bl
-##                elif row[0]=="3Cl":
-##                        ontLink = ont3Cl
-                #Locally I can just do
-                ontLink = row[0]+".png"
-                #online I do
-                ontLink = "http://ezaroukian.github.io/pdp/"+row[0]+".png"
-                image = '<br/><img src="'+ontLink+'" height="360">'
-                block = row[1]
-                complexity = row[2] 
-                truth = int(row[3])
-                NL = row[4]
-                CE = row[5]
-                ident = row[6]
-                an = row[7]
-                neg = row[8]
-                inan = row[9]
+group1 = pipeline 
+group2 = pipeline
+group3 = plain
+group4 = plain
+#update the item names after group assignments
+items1 = str(writeItems())     # hopefully this stays the same while I...
+#and add these to a names list?
+i1 = str(group1)+".Scenario8"
+i2 = str(group2)+".Scenario1"
+l1 = str(group3)+".Scenario2"
+l2 = str(group4)+".Scenario4"
+names1 = [i1, i2,  l1, l2]
+#names1ilb = [i2, i1,  l1, l2]
+#names1ilc = [i1, i2,  l2, l1]
+#names1ild = [i2, i1,  l2, l1]
+#names1lia = [l1, l2,  i1, i2]
+#names1lib = [l2, l1,  i1, i2]
+#names1lic = [l1, l2,  i2, i1]
+#names1lid = [l2, l1,  i2, i1]
 
-##                addToType = "." + ontograph + "." +  str(block) + "." + complexity + "." + str(truth)+ "." +str(counter) +"."+ident+"."+an+"."+neg+"."+inan
-##                if block == "0":
-##                        sentence01 = NL  #I arbitarily chose NL for block 1, other versions should do CE first, and should flip the blocks
-##                        itype01 = "NL_pract"+addToType
-##                        itype01c = "CE_pract"+addToType
-##                        sentence02 = CE
-##                        itype02n = "NL_pract"+addToType
-##                        itype02 = "CE_pract"+addToType
-                        
-                sentenceN = NL
-                sentenceC = CE
-                itypeN = itypeC = "." + ontograph + "." +  str(block) + "." + complexity + "." + str(truth)+ "." +str(counter)+"."+ident+"."+an+"."+neg+"."+inan
-                itypeN = "NL" + itypeN
-                itypeC = "CE" + itypeC
-                
-                if block == "0":#practice items
-                        itemN = [itypeN, "PracticeQuestion", {"q": '<span class="q">'+sentenceN+"</span>"+image, "hasCorrect": truth}  ]
-                        itemC = [itypeC, "PracticeQuestion", {"q": '<span class="q">'+sentenceC+"</span>"+image, "hasCorrect": truth}  ]
-                else:
-                        itemN = [itypeN, "Question", {"q": '<span class="q">'+sentenceN+"</span>"+image, "hasCorrect": truth } ]
-                        itemC = [itypeC, "Question", {"q": '<span class="q">'+sentenceC+"</span>"+image, "hasCorrect": truth } ]
-                        
-                items.extend([itemN,itemC])
-                if block == "0":
-                        itemsNames0n.append(itypeN)
-                        itemsNames0c.append(itypeC)
-                elif block == "1":
-                        itemsNames1n.append(itypeN)
-                        itemsNames1c.append(itypeC)
-                elif block == "2":
-                        itemsNames2n.append(itypeN)
-                        itemsNames2c.append(itypeC)
+                 
+group1 = plain
+group2 = plain
+group3 = pipeline
+group4 = pipeline
+items2 = str(writeItems())
+l1 = str(group1)+".Scenario8"
+l2 = str(group2)+".Scenario1"
+i1 = str(group3)+".Scenario2"
+i2 = str(group4)+".Scenario4"
+names2 = [i1, i2,  l1, l2]
+#names2ilb = [i2, i1,  l1, l2]
+#names2ilc = [i1, i2,  l2, l1]
+#names2ild = [i2, i1,  l2, l1]
+#names2lia = [l1, l2,  i1, i2]
+#names2lia = [l2, l1,  i1, i2]
+#names2lia = [l1, l2,  i2, i1]
+#names2lia = [l2, l1,  i2, i1]
+                 
+group1 = pipeline
+group2 = plain
+group3 = plain
+group4 = pipeline
+items3 = str(writeItems())
+i1 = str(group1)+".Scenario8"
+l2 = str(group2)+".Scenario1"
+l1 = str(group3)+".Scenario2"
+i2 = str(group4)+".Scenario4"
+names3 = [i1, i2,  l1, l2]
+#names3ilb = [i2, i1,  l1, l2]
+#names3ilc = [i1, i2,  l2, l1]
+#names3ild = [i2, i1,  l2, l1]
+#names3lia = [l1, l2,  i1, i2]
+#names3lia = [l2, l1,  i1, i2]
+#names3lia = [l1, l2,  i2, i1]
+#names3lia = [l2, l1,  i2, i1]
         
-        csvfile.close()
+group1 = plain
+group2 = pipeline
+group3 = pipeline
+group4 = plain
+items4 = str(writeItems())
+l1 = str(group1)+".Scenario8"
+i2 = str(group2)+".Scenario1"
+i1 = str(group3)+".Scenario2"
+l2 = str(group4)+".Scenario4"
+names4 = [i1, i2,  l1, l2]
+#names4ilb = [i2, i1,  l1, l2]
+#names4ilc = [i1, i2,  l2, l1]
+#names4ild = [i2, i1,  l2, l1]
+#names4lia = [l1, l2,  i1, i2]
+#names4lia = [l2, l1,  i1, i2]
+#names4lia = [l1, l2,  i2, i1]
+#names4lia = [l2, l1,  i2, i1]
+                                        
 
-with open('SUS.csv', 'rb') as csvfile:
+
+with open('SUS.csv', 'r') as csvfile:#with open('SUS.csv', 'rb') as csvfile:
         counter = 1
         sus = []
-        susNames1n = []
-        susNames1c = []
-        susNames2n = []
-        susNames2c = []
+        susNames1i = []
+        susNames1l = []
+        susNames2i = []
+        susNames2l = []
         r = csv.reader(csvfile)
-        r.next()
+        next(r)#r.next()
         for row in r:
                 order = row[0]
                 statement = row[1]
                 polarity = row[2]
                 itype = polarity+"."+order
-                itype1n = "NL.1."+itype
-                itype1c = "CE.1."+itype
-                itype2n = "NL.2."+itype
-                itype2c = "CE.2."+itype
+                itype1i = "SUS.pipeline.1."+itype
+                itype1l = "SUS.plain.2."+itype
+                itype2i = "SUS.pipeline.2."+itype
+                itype2l = "SUS.plain.2."+itype
                 #arbitrary order for lang/block right now (Latin square for which goes with which)
-                item1n = [itype1n, "AcceptabilityJudgment", {"s": statement, "q": "", "as": ["1", "2", "3", "4", "5"], "leftComment": "strongly disagree", "rightComment": "strongly agree"} ]
-                item1c = [itype1c, "AcceptabilityJudgment", {"s": statement, "q": "", "as": ["1", "2", "3", "4", "5"], "leftComment": "strongly disagree", "rightComment": "strongly agree"} ]
-                item2n = [itype2n, "AcceptabilityJudgment", {"s": statement, "q": "", "as": ["1", "2", "3", "4", "5"], "leftComment": "strongly disagree", "rightComment": "strongly agree"} ]
-                item2c = [itype2c, "AcceptabilityJudgment", {"s": statement, "q": "", "as": ["1", "2", "3", "4", "5"], "leftComment": "strongly disagree", "rightComment": "strongly agree"} ]
-                sus.extend([item1n,item1c,item2n,item2c])
-                susNames1n.append(itype1n)
-                susNames1c.append(itype1c)
-                susNames2n.append(itype2n)
-                susNames2c.append(itype2c)
+                item1i = [itype1i, "AcceptabilityJudgment", {"s": statement, "q": "", "as": ["1", "2", "3", "4", "5"], "leftComment": "strongly disagree", "rightComment": "strongly agree"} ]
+                item1l = [itype1l, "AcceptabilityJudgment", {"s": statement, "q": "", "as": ["1", "2", "3", "4", "5"], "leftComment": "strongly disagree", "rightComment": "strongly agree"} ]
+                item2l = [itype2l, "AcceptabilityJudgment", {"s": statement, "q": "", "as": ["1", "2", "3", "4", "5"], "leftComment": "strongly disagree", "rightComment": "strongly agree"} ]
+                item2i = [itype2i, "AcceptabilityJudgment", {"s": statement, "q": "", "as": ["1", "2", "3", "4", "5"], "leftComment": "strongly disagree", "rightComment": "strongly agree"} ]
+                sus.extend([item1i,item1l,item2l,item2i])
+                susNames1i.append(itype1i)
+                susNames1l.append(itype1l)
+                susNames2i.append(itype2i)
+                susNames2l.append(itype2l)
                 
         csvfile.close()
 
 
 
 with open('genItems.js', 'w') as writefile:
-        writefile.write("var newTestItems = "+str(items)+";\n\n")
-        writefile.write("var newSUSItems = "+str(sus)+";\n\n\n")
+                
+        writefile.write("var p1 = "+items1+";\n\n")
+        writefile.write("var p2 = "+items2+";\n\n")
+        writefile.write("var p3 = "+items3+";\n\n")
+        writefile.write("var p4 = "+items4+";\n\n")
+        writefile.write("var SUSItems = "+str(sus)+";\n\n\n")
 
-        writefile.write("var mySeqNC = seq('intro', 'toPractice', followEachWith('sepPractice',seq('"+"', '".join(itemsNames0n)+"')), 'endPractice', followEachWith(seq('conf','sep'),rshuffle('"+"', '".join(itemsNames1n)+"')), 'toSUS', '" + "', '".join(susNames1n)+"', 'break', 'toPractice', followEachWith('sepPractice',seq('"+"', '".join(itemsNames0c)+"')), 'endPractice', followEachWith(seq('conf','sep'),rshuffle('"+"', '".join(itemsNames2c)+"')), 'toSUS', '"+"', '".join(susNames2c)+"', 'endSurvey');\n\n")
-        writefile.write("var mySeqCN = seq('intro', 'toPractice', followEachWith('sepPractice',seq('"+"', '".join(itemsNames0c)+"')), 'endPractice', followEachWith(seq('conf','sep'),rshuffle('"+"', '".join(itemsNames1c)+"')), 'toSUS', '" + "', '".join(susNames1c)+"', 'break', 'toPractice', followEachWith('sepPractice',seq('"+"', '".join(itemsNames0n)+"')), 'endPractice', followEachWith(seq('conf','sep'),rshuffle('"+"', '".join(itemsNames2n)+"')), 'toSUS', '"+"', '".join(susNames2n)+"', 'endSurvey');")
-        
+        writefile.write("var ss1_il = seq('inst', 'trainM', sepWith('sep',shuffle('"+"', '".join(names1[0:2])+"')), 'toSUS', '" + "', '".join(susNames1i)+"', 'comments', 'trainN', sepWith('sep',shuffle('"+"', '".join(names1[2:4])+"')), 'toSUS', '"+"', '".join(susNames2l)+"', 'comments');\n\n")
+        writefile.write("var ss1_li = seq('inst', 'trainN', sepWith('sep',shuffle('"+"', '".join(names1[2:4])+"')), 'toSUS', '" + "', '".join(susNames1l)+"', 'comments', 'trainM', sepWith('sep',shuffle('"+"', '".join(names1[0:2])+"')), 'toSUS', '"+"', '".join(susNames2i)+"', 'comments');\n\n")  
+
+        writefile.write("var ss2_il = seq('inst', 'trainM', sepWith('sep',shuffle('"+"', '".join(names2[0:2])+"')), 'toSUS', '" + "', '".join(susNames1i)+"', 'comments', 'trainN', sepWith('sep',shuffle('"+"', '".join(names2[2:4])+"')), 'toSUS', '"+"', '".join(susNames2l)+"', 'comments');\n\n")
+        writefile.write("var ss2_li = seq('inst', 'trainN', sepWith('sep',shuffle('"+"', '".join(names2[2:4])+"')), 'toSUS', '" + "', '".join(susNames1l)+"', 'comments', 'trainM', sepWith('sep',shuffle('"+"', '".join(names2[0:2])+"')), 'toSUS', '"+"', '".join(susNames2i)+"', 'comments');\n\n")
+
+        writefile.write("var ss3_il = seq('inst', 'trainM', sepWith('sep',shuffle('"+"', '".join(names3[0:2])+"')), 'toSUS', '" + "', '".join(susNames1i)+"', 'comments', 'trainN', sepWith('sep',shuffle('"+"', '".join(names3[2:4])+"')), 'toSUS', '"+"', '".join(susNames2l)+"', 'comments');\n\n")
+        writefile.write("var ss3_li = seq('inst', 'trainN', sepWith('sep',shuffle('"+"', '".join(names3[2:4])+"')), 'toSUS', '" + "', '".join(susNames1l)+"', 'comments', 'trainM', sepWith('sep',shuffle('"+"', '".join(names3[0:2])+"')), 'toSUS', '"+"', '".join(susNames2i)+"', 'comments');\n\n")  
+
+        writefile.write("var ss4_il = seq('inst', 'trainM', sepWith('sep',shuffle('"+"', '".join(names4[0:2])+"')), 'toSUS', '" + "', '".join(susNames1i)+"', 'comments', 'trainN', sepWith('sep',shuffle('"+"', '".join(names4[2:4])+"')), 'toSUS', '"+"', '".join(susNames2l)+"', 'comments');\n\n")
+        writefile.write("var ss4_li = seq('inst', 'trainN', sepWith('sep',shuffle('"+"', '".join(names4[2:4])+"')), 'toSUS', '" + "', '".join(susNames1l)+"', 'comments', 'trainM', sepWith('sep',shuffle('"+"', '".join(names4[0:2])+"')), 'toSUS', '"+"', '".join(susNames2i)+"', 'comments');\n\n")  
+       
+         
+
+                
         writefile.close()
 
 
